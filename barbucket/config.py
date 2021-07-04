d44392ff15da8d69ac971f7fe9ccbef440f9364d
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-07-04 22:40:50 +0200

    Refactored 'Config' class

--- barbucket/config.py
+++ barbucket/config.py
@@ -4,60 +4,56 @@ from pathlib import Path
 
 class Config():
 
-
-    __parser = configparser.ConfigParser(allow_no_value=True)
-    __default_config = {
-        'database': {
-            '\# db location within current users home folder': '',
-            'db_location': ".barbucket/database.db"},
-        'contracts': {},
-        'quotes': {
-            'redownload_days': "5"},
-        'quality_check': {
-            'min_quotes_count': "250",
-            'max_missing_quotes_at_end': "4",
-            'max_gap_size': "4"},
-        'tws_connector': {
-            'non_systemic_codes': "162,200,354,2104,2106,2158",
-            'ip': "127.0.0.1",
-            'port': "7497",
-            '\# 7497 tws paper, 4002/4003 ibg paper/real': '', 
-            '\# Minimum number of business days until new download is started': "5"}}
+    _parser = configparser.ConfigParser(allow_no_value=True)
+    _config_file_path = None
 
 
-    def __init__(self):
-        pass
-
-
-    def init_config_file(self,):
+    def __init__(self,):
         """
-        Create file if not present and fill with default values
+        Prepare config file and read content
         """
-        path = Path.home()
-        if not Path.is_dir(path / ".barbucket"):
-            Path.mkdir(path / ".barbucket")
-        
-        if not Path.is_dir(path / ".barbucket/tw_screener"):
-            Path.mkdir(path / ".barbucket/tw_screener")
-        
-        path = path / ".barbucket/config.ini"
-        self.__parser.read_dict(self.__default_config)
-        with open(path, 'w') as file:
-            self.__parser.write(file)
+
+        self.create_directories_if_not_present()
+
+        self.set_config_file_path()
+
+        self.create_config_file_if_not_present(
+                source_path="barbucket/default_config.ini",
+                destination_path=self._config_file_path)
+
+        # Read current config file
+        self._parser.read(self._config_file_path)
+
+
+    def create_directories_if_not_present(self,):
+
+        if not Path.is_dir(Path.home() / ".barbucket"):
+            Path.mkdir(Path.home() / ".barbucket")
+        if not Path.is_dir(Path.home() / ".barbucket/tw_screener"):
+            Path.mkdir(Path.home() / ".barbucket/tw_screener")
+
+
+    def set_config_file_path(self,):
+        self._config_file_path = Path.home() / ".barbucket/config.ini"
+
+
+    def create_config_file_if_not_present(self, source_path, destination_path):
+
+        # Create file with default config if none exists
+        if not Path.is_file(destination_path):
+            with open(source_path, 'r') as reader:
+                default_config = reader.readlines()
+            with open(destination_path, 'w') as writer:
+                writer.writelines(default_config)
 
 
     def get_config_value(self, section, option):
         """
-        Read values from file
+        Read a config value
         """
-        path = Path.home() / ".barbucket/config.ini"
 
-        if not Path.is_file(path):
-            self.init_config_file()
+        value = self._parser.get(section, option)
 
-        self.__parser.read(path)
-        value = self.__parser.get(section, option)
-        
         #If value contains comma, split by comma and change to list
         if "," in value:
             value = value.split(",")
