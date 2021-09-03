06133f92f0d1fdea91d91f7e3f0ddd3a3f73fc10
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-09-03 18:24:57 +0200

    Changed order of methods

--- barbucket/ib_details_processor.py
+++ barbucket/ib_details_processor.py
@@ -18,11 +18,27 @@ class IbDetailsProcessor(BaseComponent):
         self.__details = None
         self.__pbar = None
 
-    def __setup_progress_bar(self) -> None:
-        manager = enlighten.get_manager()
-        self.__pbar = manager.counter(
-            total=len(self.__contracts),
-            desc="Contracts", unit="contracts")
+    def update_ib_contract_details(self):
+        """Docstring"""
+
+        self.__get_contracts()
+        self.__setup_progress_bar()
+        self.__connect_tws()
+        try:
+            for contract in self.__contracts:
+                if self.__check_abort_conditions():
+                    break
+                try:
+                    self.__get_contract_details_from_tws(contract)
+                except (QueryReturnedNoResultError, QueryReturnedMultipleResultsError):
+                    pass
+                else:
+                    self.__decode_exchange_names()
+                    self.__insert_ib_details_into_db(contract)
+                finally:
+                    self.__pbar.update(inc=1)
+        finally:
+            self.__disconnect_tws()
 
     def __get_contracts(self) -> None:
         """Get contracts from db, where IB details are missing"""
@@ -34,6 +50,12 @@ class IbDetailsProcessor(BaseComponent):
         self.__contracts = self.mediator.notify("get_contracts", parameters)
         logging.info(f"Found {len(self.__contracts)} contracts with missing IB "
                      f"details in master listing.")
+
+    def __setup_progress_bar(self) -> None:
+        manager = enlighten.get_manager()
+        self.__pbar = manager.counter(
+            total=len(self.__contracts),
+            desc="Contracts", unit="contracts")
 
     def __connect_tws(self) -> None:
         """Docstring"""
@@ -104,25 +126,3 @@ class IbDetailsProcessor(BaseComponent):
         conn.commit()
         cur.close()
         self.mediator.notify("close_db_connection", {'conn': conn})
-
-    def update_ib_contract_details(self):
-        """Docstring"""
-
-        self.__get_contracts()
-        self.__setup_progress_bar()
-        self.__connect_tws()
-        try:
-            for contract in self.__contracts:
-                if self.__check_abort_conditions():
-                    break
-                try:
-                    self.__get_contract_details_from_tws(contract)
-                except (QueryReturnedNoResultError, QueryReturnedMultipleResultsError):
-                    pass
-                else:
-                    self.__decode_exchange_names()
-                    self.__insert_ib_details_into_db(contract)
-                finally:
-                    self.__pbar.update(inc=1)
-        finally:
-            self.__disconnect_tws()
