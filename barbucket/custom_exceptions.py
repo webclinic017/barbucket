c95d47330cbeeea1f3c3df912a9aae6181ff931b
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-09-03 16:19:38 +0200

    Added custom exceptions file

---
+++ barbucket/custom_exceptions.py
@@ -0,0 +1,18 @@
+class ExistingDataIsSufficientError(Exception):
+    """Docstring"""
+
+
+class ExistingDataIsTooOldError(Exception):
+    """Docstring"""
+
+
+class ContractHasErrorStatusError(Exception):
+    """Docstring"""
+
+
+class QueryReturnedNoResultError(Exception):
+    """[summary]"""
+
+
+class QueryReturnedMultipleResultsError(Exception):
+    """[summary]"""
