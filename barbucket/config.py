d0a7c95ac69335d9c5d1798c97b253d9bc35295e
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-09-03 16:18:15 +0200

    Restructured config class

--- barbucket/config.py
+++ barbucket/config.py
@@ -3,55 +3,56 @@ from pathlib import Path
 from typing import List
 
 
-class Config():
+class ConfigInitializer():
+    """Docstring"""
 
-    _parser = configparser.ConfigParser(allow_no_value=True)
-    _config_file_path = None
+    _CONFIG_FILE_PATH = Path.home() / ".barbucket/config.ini"
 
-    def __init__(self,) -> None:
-        """Prepare config file and read content"""
+    def __init__(self) -> None:
+        pass
 
+    def initalize_config(self) -> None:
+        """Prepare config file, if not present"""
         self._create_directories()
-        self._set_config_file_path()
-        self._create_config_file_if_not_present(
+        self._create_config_file(
             source_path="default_config.ini",
-            destination_path=self._config_file_path)
+            destination_path=self._CONFIG_FILE_PATH)
 
-        # Read current config file
-        self._parser.read(self._config_file_path)
-
-    def _create_directories(self,) -> None:
-        # Create both directories if not present, throws no exception if directories already present
+    def _create_directories(self) -> None:
+        """Create both directories if not present, throws no exception if 
+        directories already present"""
         Path.mkdir(
-            (Path.home() / ".barbucket/tw_screener"),
+            (Path.home() / ".barbucket/tv_screener"),
             parents=True,
             exist_ok=True)
 
-    def _set_config_file_path(self,) -> None:
-        self._config_file_path = Path.home() / ".barbucket/config.ini"
-
-    def _create_config_file_if_not_present(
-        self, source_path: Path,
-        destination_path: Path
-    ) -> None:
-        # Create file with default config if none exists
+    def _create_config_file(
+            self, source_path: Path,
+            destination_path: Path) -> None:
+        """Create file with default config if none exists"""
         if not Path.is_file(destination_path):
             with open(source_path, 'r') as reader:
                 default_config = reader.readlines()
             with open(destination_path, 'w') as writer:
                 writer.writelines(default_config)
 
+
+class ConfigReader():
+    """Docstring"""
+
+    _PARSER = configparser.ConfigParser(allow_no_value=True)
+    _CONFIG_FILE_PATH = Path.home() / ".barbucket/config.ini"
+
+    def __init__(self) -> None:
+        """Read config file content"""
+        self._PARSER.read(self._CONFIG_FILE_PATH)
+
     def get_config_value_single(self, section: str, option: str) -> str:
         """Read a single config value"""
-
-        return self._parser.get(section, option)
+        return self._PARSER.get(section, option)
 
     def get_config_value_list(self, section: str, option: str) -> List[str]:
         """Read a config value list"""
-
-        value = self._parser.get(section, option)
-
-        # split by comma and change to list
-        value = value.split(",")
-
+        value = self._PARSER.get(section, option)
+        value = value.split(",")  # split by comma and change to list
         return value
