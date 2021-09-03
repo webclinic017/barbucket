fecfd36849dc8c29208aa7ec2847cb80ae1c40e8
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-09-03 20:10:12 +0200

    Restructured contracts classes for mediator

--- barbucket/contracts.py
+++ barbucket/contracts.py
@@ -6,31 +6,29 @@ from bs4 import BeautifulSoup
 import requests
 import enlighten
 
-from .database import DatabaseConnector
-from .tools import GracefulExiter
+from .mediator import Mediator
 
 
 class ContractsDatabase():
 
-    def __init__(self):
-        pass
+    def __init__(self, mediator: Mediator = None) -> None:
+        self.mediator = mediator
 
     def create_contract(self, contract_type_from_listing, exchange_symbol,
                         broker_symbol, name, currency, exchange):
-        logging.debug(
-            f"Creating new contract {contract_type_from_listing}_{exchange}_{broker_symbol}_{currency}.")
-        db_connector = DatabaseConnector()
-        conn = db_connector.connect()
+        logging.debug(f"Creating new contract {contract_type_from_listing}_"
+                      f"{exchange}_{broker_symbol}_{currency}.")
+        conn = self.mediator.notify("get_db_connection", {})
         cur = conn.cursor()
 
         cur.execute(
             """INSERT INTO contracts (
                     contract_type_from_listing,
-                    exchange_symbol, 
+                    exchange_symbol,
                     broker_symbol,
                     name,
                     currency,
-                    exchange) 
+                    exchange)
                     VALUES (?, ?, ?, ?, ?, ?)""",
             (contract_type_from_listing,
                 exchange_symbol,
@@ -43,12 +41,10 @@ class ContractsDatabase():
 
         conn.commit()
         cur.close()
-        db_connector.disconnect(conn)
+        self.mediator.notify("close_db_connection", {'conn': conn})
 
     def get_contracts(self, filters={}, return_columns=[]):
-        """
-        returns a list of sqlite3.Row objects
-        """
+        """returns a list of sqlite3.Row objects"""
 
         # Prepare query to get requested values from db
         query = "SELECT * FROM all_contract_info"
@@ -73,8 +69,7 @@ class ContractsDatabase():
 
         # Get requested values from db
         logging.debug(f"Getting contracts from databse with query: {query}")
-        db_connector = DatabaseConnector()
-        conn = db_connector.connect()
+        conn = self.mediator.notify("get_db_connection", {})
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
 
@@ -83,19 +78,18 @@ class ContractsDatabase():
 
         conn.commit()
         cur.close()
-        db_connector.disconnect(conn)
+        self.mediator.notify("close_db_connection", {'conn': conn})
 
         return contracts
 
     def delete_contract(self, exchange, symbol, currency):
-        db_connector = DatabaseConnector()
-        conn = db_connector.connect()
+        conn = self.mediator.notify("get_db_connection", {})
         cur = conn.cursor()
 
         cur.execute(
             """DELETE FROM contracts
-                    WHERE (broker_symbol = ? 
-                        AND exchange = ? 
+                    WHERE (broker_symbol = ?
+                        AND exchange = ?
                         AND currency = ?);""",
             (symbol,
              exchange,
@@ -103,104 +97,62 @@ class ContractsDatabase():
 
         conn.commit()
         cur.close()
-        db_connector.disconnect(conn)
+        self.mediator.notify("close_db_connection", {'conn': conn})
 
     def delete_contract_id(self, contract_id):
-        db_connector = DatabaseConnector()
-        conn = db_connector.connect()
+        conn = self.mediator.notify("get_db_connection", {})
         cur = conn.cursor()
 
         cur.execute(
-            """DELETE FROM contracts 
+            """DELETE FROM contracts
                     WHERE contract_id = ?;""",
             contract_id)
 
         conn.commit()
         cur.close()
-        db_connector.disconnect(conn)
+        self.mediator.notify("close_db_connection", {'conn': conn})
 
 
 class IbExchangeListings():
 
-    def __init__(self):
-        pass
+    def __init__(self, mediator: Mediator = None) -> None:
+        self.mediator = mediator
+        self.__ctype = None
+        self.__exchange = None
 
     def sync_contracts_to_listing(self, ctype, exchange):
-        logging.info(
-            f'Syncing {ctype} contracts on {exchange} to master listing.')
+        logging.info(f"Syncing {ctype} contracts on {exchange} to master "
+                     f"listing.")
+
+        self.__ctype = ctype
+        self.__exchange = exchange
 
         # Get all contracts from websites
-        website_data = []
         if ctype == "ETF":
-            website_data = self.ib_listings.read_ib_exchange_listing_singlepage(
-                ctype, exchange)
+            website_data = self.__read_ib_exchange_listing_singlepage()
         elif ctype == "STOCK":
-            website_data = self.ib_listings.read_ib_exchange_listing_paginated(
-                ctype, exchange)
+            website_data = self.__read_ib_exchange_listing_paginated()
+        # Todo: SOLID
 
         # Abort, if webscraping was aborted by user
         if website_data == []:
             return
         # Todo: Exception
 
-        # Todo: Call mediator
         # Get all contracts from database
         filters = {'contract_type_from_listing': ctype, 'exchange': exchange}
         columns = ['broker_symbol', 'currency']
-        database_data = self.contracts_db.get_contracts(
-            filters=filters,
-            return_columns=columns)
+        database_data = self.mediator.notify(
+            "get_contracts",
+            {'filters': filters,
+             'return_columns': columns})
 
-    def __delete_contracts_from_db():
-        # Delete contracts from database, that are not present in website
-        contracts_removed = 0
-        for db_row in database_data:
-            exists = False
-            for web_row in website_data:
-                if (
-                    (db_row['broker_symbol'] == web_row['broker_symbol'])
-                    and
-                    (db_row['currency'] == web_row['currency'])
-                ):
-                    exists = True
-                    break
-            if not exists:
-                self.contracts_db.delete_contract(
-                    symbol=db_row['broker_symbol'],
-                    exchange=exchange.upper(),
-                    currency=db_row['currency'])
-                contracts_removed += 1
-        logging.info(
-            f'{contracts_removed} contracts removed from master listing.')
+        self.__remove_deleted_contracts_from_db(website_data, database_data)
+        self.__add_new_contracts_to_db(website_data, database_data)
 
-    def __add_contracts_to_db():
-
-        # Add contracts from website to database, that are not present in database
-        contracts_added = 0
-        for web_row in website_data:
-            exists = False
-            for db_row in database_data:
-                if (
-                    (web_row['broker_symbol'] == db_row['broker_symbol'])
-                    and
-                    (web_row['currency'] == db_row['currency'])
-                ):
-                    exists = True
-                    break
-            if not exists:
-                self.contracts_db.create_contract(
-                    contract_type_from_listing=ctype,
-                    exchange_symbol=web_row['exchange_symbol'],
-                    broker_symbol=web_row['broker_symbol'],
-                    name=web_row['name'],
-                    currency=web_row['currency'],
-                    exchange=exchange.upper())
-                contracts_added += 1
-        logging.info(f'{contracts_added} contracts added to master listing.')
-
-    def __read_ib_exchange_listing_singlepage(self, ctype, exchange):
-        url = f"https://www.interactivebrokers.com/en/index.php?f=567"\
-            + f"&exch={exchange}"
+    def __read_ib_exchange_listing_singlepage(self):
+        url = (f"https://www.interactivebrokers.com/en/index.php?f=567&exch="
+               f"{self.__exchange}")
         html = requests.get(url).text
 
         # Correct error from IB
@@ -231,17 +183,17 @@ class IbExchangeListings():
         for row in rows:
             cols = row.find_all('td')
             row_dict = {
-                'type': ctype,
+                'type': self.__ctype,
                 'broker_symbol': cols[0].text.strip(),
                 'name': cols[1].text.strip(),
                 'exchange_symbol': cols[2].text.strip(),
                 'currency': cols[3].text.strip(),
-                'exchange': exchange.upper()}
+                'exchange': self.__exchange.upper()}
             website_data.append(row_dict)
 
         return website_data
 
-    def __read_ib_exchange_listing_paginated(self, ctype, exchange):
+    def __read_ib_exchange_listing_paginated(self):
         """
         Returns list of contracts
         Returns -1 if aborted by user
@@ -254,15 +206,13 @@ class IbExchangeListings():
         # manager = enlighten.get_manager()
         # pbar = manager.counter(total=len(contracts), desc="Contracts", unit="contracts")
 
-        exiter = GracefulExiter()
-
         while True:
             # Get website
             logging.info(
-                f"Scraping IB exchange listing for {exchange}, page {page}.")
-            url = f"https://www.interactivebrokers.com/en/index.php?f=2222"\
-                + f"&exch={exchange}&showcategories=STK&p=&cc=&limit=100"\
-                + f"&page={page}"
+                f"Scraping IB exchange listing for {self.__exchange}, page {page}.")
+            url = (f"https://www.interactivebrokers.com/en/index.php?f=2222"
+                   f"&exch={self.__exchange}&showcategories=STK&p=&cc=&limit="
+                   f"100&page={page}")
             html = requests.get(url).text
 
             # Correct error from IB
@@ -289,19 +239,69 @@ class IbExchangeListings():
             for row in rows:
                 cols = row.find_all('td')
                 row_dict = {
-                    'type': ctype,
+                    'type': self.__ctype,
                     'broker_symbol': cols[0].text.strip(),
                     'name': cols[1].text.strip(),
                     'exchange_symbol': cols[2].text.strip(),
                     'currency': cols[3].text.strip(),
-                    'exchange': exchange.upper()}
+                    'exchange': self.__exchange.upper()}
                 website_data.append(row_dict)
 
-            # Check for abort signal
-            if exiter.exit():
+            # Check for Ctrl-C signal
+            if self.mediator.notify("exit_signal"):
                 logging.info(f"Exiting on user request.")
                 return []
 
             # Prepare for next page
             page += 1
-            time.sleep(3)  # show some mercy to webserver
+            time.sleep(3)  # show some mercy to IB webserver
+
+    def __remove_deleted_contracts_from_db(self, website_data, database_data):
+        """Delete contracts from database, that are not present in website"""
+
+        contracts_removed = 0
+        for db_row in database_data:
+            exists = False
+            for web_row in website_data:
+                if (
+                    (db_row['broker_symbol'] == web_row['broker_symbol'])
+                    and
+                    (db_row['currency'] == web_row['currency'])
+                ):
+                    exists = True
+                    break
+            if not exists:
+                self.mediator.notify(
+                    "delete_contract",
+                    {'symbol': db_row['broker_symbol'],
+                     'exchange': self.__exchange.upper(),
+                     'currency': db_row['currency']})
+                contracts_removed += 1
+        logging.info(
+            f"{contracts_removed} contracts removed from master listing.")
+
+    def __add_new_contracts_to_db(self, website_data, database_data):
+        """Add contracts from website to database, that are not present in database"""
+
+        contracts_added = 0
+        for web_row in website_data:
+            exists = False
+            for db_row in database_data:
+                if (
+                    (web_row['broker_symbol'] == db_row['broker_symbol'])
+                    and
+                    (web_row['currency'] == db_row['currency'])
+                ):
+                    exists = True
+                    break
+            if not exists:
+                self.mediator.notify(
+                    "create_contract",
+                    {'contract_type_from_listing': self.__ctype,
+                     'exchange_symbol': web_row['exchange_symbol'],
+                     'broker_symbol': web_row['broker_symbol'],
+                     'name': web_row['name'],
+                     'currency': web_row['currency'],
+                     'exchange': self.__exchange.upper()})
+                contracts_added += 1
+        logging.info(f'{contracts_added} contracts added to master listing.')
