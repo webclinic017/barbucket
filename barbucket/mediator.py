cd265b5bb0aa2c8dc5a9d32e7c1dc31b932569f3
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-09-03 18:24:12 +0200

    added more calls to mediator class

--- barbucket/mediator.py
+++ barbucket/mediator.py
@@ -38,6 +38,7 @@ class Mediator():
         # Instanciate components
         self.__db_initializer = db_initializer
         self.__db_initializer.mediator = self
+
         self.__db_connector = db_connector
         self.__db_connector.mediator = self
 
@@ -55,6 +56,7 @@ class Mediator():
 
         self.__quotes_db = quotes_db
         self.__quotes_db.mediator = self
+
         self.__quotes_status_db = quotes_status_db
         self.__quotes_status_db.mediator = self
 
@@ -66,6 +68,7 @@ class Mediator():
 
         self.__config_initializer = config_initializer
         self.__config_initializer.mediator = self
+
         self.__config_reader = config_reader
         self.__config_reader.mediator = self
 
@@ -76,19 +79,25 @@ class Mediator():
         self.__tools.mediator = self
 
     def notify(self, action: str, parameters: dict = {}) -> Optional[object]:
+        # ContractsDatabase
         if action == "get_contracts":
             return self.__contracts_db.get_contracts(
                 filters=parameters['filters'],
                 return_columns=parameters['return_columns'])
-
+        # DatabaseInitializer
+        elif action == "initialize_database":
+            return self.__db_initializer.initialize_database()
+        # DatabaseConnector
         elif action == "get_db_connection":
             return self.__db_connector.connect()
         elif action == "close_db_connection":
-            return self.__db_connector.disconnect(conn=parameters['conn'])
+            return self.__db_connector.disconnect(
+                conn=parameters['conn'])
+        # Tools
         elif action == "decode_exchange_tv":
-            return self.__db_connector.connect(
+            return self.__tools.decode_exchange_tv(
                 exchange=parameters['exchange'])
-
+        # Tws
         elif action == "download_contract_details_from_tws":
             return self.__tws.download_contract_details(
                 contract_type_from_listing=parameters['contract_type_from_listing'],
@@ -108,11 +117,15 @@ class Mediator():
                 exchange=parameters['exchange'],
                 currency=parameters['currency'],
                 duration=parameters['duration'])
-        elif action == "exit_signal":
-            return self.__exiter.exit()
         elif action == "get_tws_contract_error":
             return self.__tws.get_contract_error()
-
+        # GracefulExiter
+        elif action == "exit_signal":
+            return self.__exiter.exit()
+        # ConfigInitializer
+        elif action == "initalize_config":
+            return self.__config_initializer.initalize_config()
+        # ConfigReader
         elif action == "get_config_value_single":
             return self.__config_reader.get_config_value_single(
                 section=parameters['section'],
@@ -121,17 +134,39 @@ class Mediator():
             return self.__config_reader.get_config_value_list(
                 section=parameters['section'],
                 option=parameters['option'])
-
+        # UniversesDatabase
         elif action == "get_universe_members":
             return self.__universe_db.get_universe_members(
                 universe=parameters['universe'])
-
+        elif action == "create_universe":
+            return self.__universe_db.create_universe(
+                name=parameters['name'],
+                contract_ids=parameters['contract_ids'])
+        elif action == "get_universes":
+            return self.__universe_db.get_universes()
+        elif action == "delete_universe":
+            return self.__universe_db.delete_universe(
+                universe=parameters['universe'])
+        # QuotesDatabase
         elif action == "insert_quotes":
-            self.__quotes_db.insert_quotes(quotes=parameters['quotes'])
+            return self.__quotes_db.insert_quotes(
+                quotes=parameters['quotes'])
+        # QuotesStatusDatabase
         elif action == "insert_quotes_status":
-            self.__quotes_status_db.insert_quotes_status(
+            return self.__quotes_status_db.insert_quotes_status(
                 contract_id=parameters['contract_id'],
                 status_code=parameters['status_code'],
                 status_text=parameters['status_text'],
                 daily_quotes_requested_from=parameters['daily_quotes_requested_from'],
                 daily_quotes_requested_till=parameters['daily_quotes_requested_till'])
+        # IbExchangeListings
+        elif action == "sync_contracts_to_listing":
+            return self.__ib_listings.sync_contracts_to_listing(
+                ctype=parameters['ctype'],
+                exchange=parameters['exchange'])
+        # IbDetailsProc
+        elif action == "update_ib_contract_details":
+            return self.__ib_details_processor.update_ib_contract_details()
+        # TvDetailsProc
+        elif action == "read_tv_data":
+            return self.__tv_details_processor.read_tv_data()
