97a253009f5f00ee8cc26440668d17ce773990d3
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-09-17 22:06:38 +0200

    Changed formatting

--- barbucket/db_connector.py
+++ barbucket/db_connector.py
@@ -17,9 +17,8 @@ class DbConnector():
 
         self.__get_db_path()
         if not self._DB_PATH.is_file():
-            raise DbNotInitializedError("Database does not exist. Call "
-                                        "'init_database()' before connecting.")
-            # otherwise connect() would create an empty file
+            raise DbNotInitializedError("Database file does not exist.")
+            # otherwise sqlite3.connect() would create an empty file and we dont want that
         conn = sqlite3.connect(self._DB_PATH)
         conn.execute("""
             PRAGMA foreign_keys = 1;
@@ -28,12 +27,12 @@ class DbConnector():
         # so this needs to be part of every db connection.
         return conn
 
-    def disconnect(self, conn: sqlite3.Connection) -> None:
-        """Disconnects the connection to the database."""
-        conn.close()
-
     def __get_db_path(self) -> None:
         conf_path = self.mediator.notify(
             "get_config_value_single",
             {'section': "database", 'option': "db_location"})
         self._DB_PATH = Path.home() / Path(conf_path)
+
+    def disconnect(self, conn: sqlite3.Connection) -> None:
+        """Disconnects the connection to the database."""
+        conn.close()
