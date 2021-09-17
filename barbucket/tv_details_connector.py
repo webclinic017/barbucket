45547dfb94bef23153e0241f7a0d1ca32f48f4ff
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-09-17 22:16:08 +0200

    changed formatting

--- barbucket/tv_details_connector.py
+++ barbucket/tv_details_connector.py
@@ -11,22 +11,24 @@ class TvDetailsDbConnector():
     def __init__(self, mediator: Mediator = None) -> None:
         self.mediator = mediator
 
-    def insert_tv_details(self, contract_id: int, market_cap: int,
-                          avg_vol_30_in_curr: int, country: str,
-                          employees: int, profit: int, revenue: int) -> None:
+    def insert_tv_details(
+            self, contract_id: int, market_cap: int, avg_vol_30_in_curr: int,
+            country: str, employees: int, profit: int, revenue: int) -> None:
         """Writing tv details to db"""
 
         conn = self.mediator.notify("get_db_connection", {})
         cur = conn.cursor()
-        cur.execute("""REPLACE INTO contract_details_tv (
-            contract_id,
-            market_cap,
-            avg_vol_30_in_curr,
-            country,
-            employees,
-            profit,
-            revenue)
-            VALUES (?, ?, ?, ?, ?, ?, ?)""", (
+        cur.execute("""
+            REPLACE INTO contract_details_tv (
+                contract_id,
+                market_cap,
+                avg_vol_30_in_curr,
+                country,
+                employees,
+                profit,
+                revenue)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+            """, (
             contract_id,
             market_cap,
             avg_vol_30_in_curr,
@@ -37,4 +39,4 @@ class TvDetailsDbConnector():
         conn.commit()
         cur.close()
         self.mediator.notify("close_db_connection", {'conn': conn})
-        logger.info("Writing tv details for contract_id {contract_id} to db.")
+        logger.debug("Wrote tv details for contract_id {contract_id} to db.")
