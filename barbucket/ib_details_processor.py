6d4446cdb04f89b0cc183d5d380dac729c966388
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-09-17 22:11:27 +0200

    refactored class

--- barbucket/ib_details_processor.py
+++ barbucket/ib_details_processor.py
@@ -1,25 +1,30 @@
 import logging
-from typing import Any
+from typing import Any, List
 
 import enlighten
 
 from .mediator import Mediator
-from .custom_exceptions import QueryReturnedNoResultError
-from .custom_exceptions import QueryReturnedMultipleResultsError
 from .base_component import BaseComponent
 from .encoder import Encoder
+from .signal_handler import SignalHandler
+from .custom_exceptions import (
+    QueryReturnedNoResultError,
+    QueryReturnedMultipleResultsError,
+    TwsSystemicError,
+    TwsContractRelatedError)
 
 logger = logging.getLogger(__name__)
 
 
 class IbDetailsProcessor(BaseComponent):
-    """Processing of contract details provided by IB TWS"""
+    """Downloading of contract details from IB TWS and storing to db"""
 
     def __init__(self, mediator: Mediator = None) -> None:
-        self.mediator = mediator
-        self.__contracts = None
-        self.__details = None
-        self.__pbar = None
+        self.mediator: Mediator = mediator
+        self.__contracts: List[Any] = None
+        self.__details: Any = None
+        self.__pbar: Any = None
+        self.__signal_handler = SignalHandler()
 
         # Setup progress bar
         manager = enlighten.get_manager()
@@ -36,33 +41,37 @@ class IbDetailsProcessor(BaseComponent):
         try:
             for contract in self.__contracts:
                 if self.__check_abort_conditions():
-                    break
+                    break  # todo: should be an exception to handle it better
                 try:
                     self.__get_contract_details_from_tws(contract)
-                except QueryReturnedNoResultError:
-                    logger.warn(f"Details query for contract {contract} "
-                                f"returned no results.")
-                except QueryReturnedMultipleResultsError:
-                    logger.warn(f"Details query for contract {contract} "
-                                f"returned multiple results.")
+                    self.__validate_details()
+                except TwsContractRelatedError as e:
+                    self.__handle_tws_contract_related_error(e)
+                except QueryReturnedNoResultError as e:
+                    self.__handle_no_result_error(e)
+                except QueryReturnedMultipleResultsError as e:
+                    self.__handle_multiple_results_error(e)
                 else:
                     self.__decode_exchange_names()
                     self.__insert_ib_details_into_db(contract)
                 finally:
                     self.__pbar.update(inc=1)
+        except TwsSystemicError as e:
+            self.__handle_tws_systemic_error(e)
         finally:
             self.__disconnect_tws()
 
     def __get_contracts(self) -> None:
         """Get contracts from db, where IB details are missing"""
 
-        columns = ['contract_id', 'contract_type_from_listing',
-                   'broker_symbol', 'exchange', 'currency']
+        columns = [
+            'contract_id', 'contract_type_from_listing', 'broker_symbol',
+            'exchange', 'currency']
         filters = {'primary_exchange': "NULL"}
         parameters = {'filters': filters, 'return_columns': columns}
         self.__contracts = self.mediator.notify("get_contracts", parameters)
-        logger.info(f"Found {len(self.__contracts)} contracts with missing "
-                    f"IB details in master listing.")
+        logger.debug(f"Found {len(self.__contracts)} contracts with missing "
+                     f"IB details in master listing.")
 
     def __connect_tws(self) -> None:
         """Connect to TWS app"""
@@ -75,7 +84,7 @@ class IbDetailsProcessor(BaseComponent):
     def __check_abort_conditions(self) -> bool:
         """Check conditions to abort operation."""
 
-        if (self.mediator.notify("exit_requested_by_user")
+        if (self.__signal_handler.exit_requested()
                 or self.mediator.notify("tws_has_error")):
             return True
         else:
@@ -89,14 +98,16 @@ class IbDetailsProcessor(BaseComponent):
             'broker_symbol': contract['broker_symbol'],
             'exchange': contract['exchange'],
             'currency': contract['currency']}
-        details = self.mediator.notify(
+        self.__details = self.mediator.notify(
             "download_contract_details_from_tws", parameters)
-        if len(details) == 0:
+
+    def __validate_details(self) -> None:
+        if len(self.__details) == 0:
             raise QueryReturnedNoResultError
-        elif len(details) > 1:
+        elif len(self.__details) > 1:
             raise QueryReturnedMultipleResultsError
         else:
-            self.__details = details[0]
+            self.__details = self.__details[0]
 
     def __decode_exchange_names(self) -> None:
         """Decode exchange names"""
@@ -114,3 +125,35 @@ class IbDetailsProcessor(BaseComponent):
             'industry': self.__details['industry'],
             'category': self.__details['category'],
             'subcategory': self.__details['subcategory)']})
+
+    def __handle_tws_contract_related_error(self, e):
+        logger.warning(
+            f"Contract-related problem in TWS detected: {e.req_id}, "
+            f"{e.contract}, {e.error_code}, {e.error_string}")
+        self.mediator.notify(
+            "show_cli_message",
+            {'message': (
+                f"Details request {e.req_id} for contract {e.contract} "
+                f"returned error {e.error_code}: {e.error_string}")})
+
+    def __handle_tws_systemic_error(self, e):
+        logger.error(
+            f"Systemic problem in TWS connection detected: {e.req_id}, "
+            f"{e.contract}, {e.error_code}, {e.error_string}")
+        self.mediator.notify(
+            "show_cli_message",
+            {'message': (
+                f"Details request {e.req_id} for contract{e.contract} "
+                f"returned error {e.error_code}: {e.error_string}")})
+
+    def __handle_no_result_error(self, e):
+        logger.warning(f"__handle_no_result_error")
+        self.mediator.notify(
+            "show_cli_message",
+            {'message': "__handle_no_result_error"})
+
+    def __handle_multiple_results_error(self, e):
+        logger.warning(f"__handle_multiple_results_error")
+        self.mediator.notify(
+            "show_cli_message",
+            {'message': "__handle_multiple_results_error"})
