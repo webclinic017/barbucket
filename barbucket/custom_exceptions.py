127a3a22633c3012012cccc0c878a6f3ab645d8d
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-09-17 22:06:16 +0200

    Added 3 exceptions

--- barbucket/custom_exceptions.py
+++ barbucket/custom_exceptions.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class ExistingDataIsSufficientError(Exception):
     """Docstring"""
 
@@ -20,3 +23,29 @@ class QueryReturnedMultipleResultsError(Exception):
 
 class DbNotInitializedError(Exception):
     """Custom exception for connecting to a non-present database."""
+
+
+class TwsSystemicError(Exception):
+    """[summary]"""
+
+    def __init__(self, req_id: int, error_code: int, error_string: str,
+                 contract: Any) -> None:
+        self.req_id = req_id
+        self.error_code = error_code
+        self.error_string = error_string
+        self.contract = contract
+
+
+class TwsContractRelatedError(Exception):
+    """My custom exception. Parameters are also available."""
+
+    def __init__(self, req_id: int, error_code: int, error_string: str,
+                 contract: Any) -> None:
+        self.req_id = req_id
+        self.error_code = error_code
+        self.error_string = error_string
+        self.contract = contract
+
+
+class ExitSignalDetectedError(Exception):
+    """"Doc"""
