8025fb28ac1b2dc5974b4d945dfb0facf221df2c
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-09-19 18:48:42 +0200

    Corrected class name

--- barbucket.py
+++ barbucket.py
@@ -13,7 +13,7 @@ from barbucket.universes_db_connector import UniversesDbConnector
 from barbucket.quotes_db_connector import QuotesDbConnector
 from barbucket.quotes_status_db_connector import QuotesStatusDbConnector
 from barbucket.ib_details_connector import IbDetailsConnector
-from barbucket.tv_details_connector import TvDetailsConnector
+from barbucket.tv_details_connector import TvDetailsDbConnector
 from barbucket.tws_connector import TwsConnector
 from barbucket.ib_exchange_listings_processor import IbExchangeListingsProcessor
 from barbucket.ib_details_processor import IbDetailsProcessor
@@ -57,7 +57,7 @@ if __name__ == '__main__':
         quotes_db_connector=QuotesDbConnector(),
         quotes_status_db_connector=QuotesStatusDbConnector(),
         ib_details_connector=IbDetailsConnector(),
-        tv_details_connector=TvDetailsConnector(),
+        tv_details_connector=TvDetailsDbConnector(),
         tws_connector=TwsConnector(),
         ib_listings_processor=IbExchangeListingsProcessor(),
         ib_details_processor=IbDetailsProcessor(),
