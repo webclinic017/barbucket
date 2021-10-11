6b92067fa117ceb0219abf25a83bee4a8a6ec6c2
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-10-11 12:25:17 +0200

    changed console output to logging

--- barbucket/signal_handler.py
+++ barbucket/signal_handler.py
@@ -17,13 +17,11 @@ class SignalHandler():
     def change_state(self, signum: Any, frame: Any) -> None:
         """Interrupt method, called by system, when Ctrl-C is detected."""
 
-        self.mediator.notify(
-            "show_cli_message",
-            {'message': "Ctrl-C detected, gracefully stopping operation. "
-                        "Press again to stop immediately."})
+        logger.warn(
+            f"Ctrl-C detected, gracefully stopping operation. Press again to "
+            f"stop immediately.")
         signal.signal(signal.SIGINT, signal.SIG_DFL)
         self.state = True
-        logger.info(f"Exit signal detected.")
 
     def exit_requested(self) -> bool:
         """Check if the user pressed Ctrl-C."""
