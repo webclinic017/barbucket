f97b05b464414b81f1b5d2b475cb741acf9784fa
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-10-17 20:38:18 +0200

    removed comment

--- barbucket/ib_exchange_listing_reader.py
+++ barbucket/ib_exchange_listing_reader.py
@@ -170,7 +170,6 @@ class IbExchangeListingMultipageReader(IbExchangeListingReader):
 
     def __check_abort_signal(self) -> None:
         if self.__signal_handler.exit_requested():
-            # Todo: CHANGED!
             raise ExitSignalDetectedError("Message")
 
 
