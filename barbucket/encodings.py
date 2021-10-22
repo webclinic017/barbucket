e4a65ea85eff3a62057a34d9aae8f68dd5222d4b
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2021-10-22 13:35:30 +0200

    modified values

--- barbucket/encodings.py
+++ barbucket/encodings.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class Api(Enum):
+    """Doc"""
     IB = 1
     TV = 2
 
@@ -11,7 +12,7 @@ class Encoding(Enum):
 
     @classmethod
     def encode(cls, name: str, to_api: Api) -> str:
-        """Encode from Barbucket notation to specific api notation"""
+        """Encode from Barbucket notation to specific API notation"""
 
         for element in cls:
             if element.name == name:
@@ -20,7 +21,7 @@ class Encoding(Enum):
 
     @classmethod
     def decode(cls, name: str, from_api: Api) -> str:
-        """Decode from specific api notation to Barbucket notation"""
+        """Decode from specific API notation to Barbucket notation"""
 
         for element in cls:
             if element.value[from_api] == name:
@@ -29,8 +30,10 @@ class Encoding(Enum):
 
 
 class Exchange(Encoding):
+    """Doc"""
+
     AEB = {Api.IB: 'AEB', Api.TV: ''}
-    AMEX = {Api.IB: 'AMEX', Api.TV: 'AMEX'}
+    AMEX = {Api.IB: 'AMEX', Api.TV: ''}
     ARCA = {Api.IB: 'ARCA', Api.TV: 'NYSE ARCA'}
     ATH = {Api.IB: 'ATH', Api.TV: ''}
     BATS = {Api.IB: 'BATS', Api.TV: ''}
@@ -43,42 +46,44 @@ class Exchange(Encoding):
     FWB = {Api.IB: 'FWB', Api.TV: 'FWB'}
     GETTEX = {Api.IB: 'GETTEX', Api.TV: ''}
     HEX = {Api.IB: 'HEX', Api.TV: ''}
-    IBIS = {Api.IB: 'IBIS', Api.TV: 'XETR'}
-    IBIS2 = {Api.IB: 'IBIS2', Api.TV: ''}
     ISED = {Api.IB: 'ISED', Api.TV: ''}
     LSE = {Api.IB: 'LSE', Api.TV: ''}
-    LSEETF = {Api.IB: 'LSEETF', Api.TV: ''}
+    LSE_ETF = {Api.IB: 'LSEETF', Api.TV: ''}
     NASDAQ = {Api.IB: 'ISLAND', Api.TV: 'NASDAQ'}
     NYSE = {Api.IB: 'NYSE', Api.TV: 'NYSE'}
     SBF = {Api.IB: 'SBF', Api.TV: ''}
     SWB = {Api.IB: 'SWB', Api.TV: ''}
     VSE = {Api.IB: 'VSE', Api.TV: ''}
+    XETRA = {Api.IB: 'IBIS', Api.TV: 'XETR'}
+    XETRA_2 = {Api.IB: 'IBIS2', Api.TV: ''}
 
     @classmethod
     def decode(cls, name: str, from_api: Api) -> str:
         """Decode from specific api notation to Barbucket notation"""
 
-        # IB inconsistently uses the names 'island' and 'nasdaq'
+        # IB inconsistently uses the names 'ISLAND' and 'NASDAQ'
         if (from_api == Api.IB) and (name == 'NASDAQ'):
             name = 'ISLAND'
         return super().decode(name=name, from_api=from_api)
 
 
 class ContractType(Encoding):
-    COMMON_STOCK = {Api.IB: 'COMMON', Api.TV: ''}
-    ETF = {Api.IB: 'ETF', Api.TV: ''}
-    ETC = {Api.IB: 'ETC', Api.TV: ''}
-    ETN = {Api.IB: 'ETN', Api.TV: ''}
-    ETP = {Api.IB: 'ETP', Api.TV: ''}
+    """Doc"""
+
     ADR = {Api.IB: 'ADR', Api.TV: ''}
     BOND = {Api.IB: 'BOND', Api.TV: ''}
     CLOSED_END_FUND = {Api.IB: 'CLOSED_END_FUND', Api.TV: ''}
+    COMMON_STOCK = {Api.IB: 'COMMON', Api.TV: ''}
     CONV_PREFERRED = {Api.IB: 'CONVPREFERRED', Api.TV: ''}
     DUTCH_CERT = {Api.IB: 'DUTCH CERT', Api.TV: ''}
+    ETC = {Api.IB: 'ETC', Api.TV: ''}
+    ETF = {Api.IB: 'ETF', Api.TV: ''}
+    ETN = {Api.IB: 'ETN', Api.TV: ''}
+    ETP = {Api.IB: 'ETP', Api.TV: ''}
     FUND_OF_FUNDS = {Api.IB: 'FUND OF FUNDS', Api.TV: ''}
     GDR = {Api.IB: 'GDR', Api.TV: ''}
     GERMAN_CERT = {Api.IB: 'GERMAN CERT', Api.TV: ''}
-    LDT_PART = {Api.IB: 'LDT PART', Api.TV: ''}
+    LTD_PART = {Api.IB: 'LTD PART', Api.TV: ''}
     MLP = {Api.IB: 'MLP', Api.TV: ''}
     NY_REG_SHRS = {Api.IB: 'NY REG SHRS', Api.TV: ''}
     OPEN_END_FUND = {Api.IB: 'OPEN-END FUND', Api.TV: ''}
