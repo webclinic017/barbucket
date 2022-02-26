f279191660347a67df6e6e19b8256d96bfdb1725
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2022-02-26 02:09:17 +0100

    improved tests

--- tests/test_config_reader.py
+++ tests/test_config_reader.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Generator
+from shutil import copyfile
 
 import pytest
 
@@ -8,32 +9,39 @@ from barbucket.config_reader import ConfigReader
 
 @pytest.fixture
 def filepath(tmp_path: Path) -> Generator:
-    filepath = tmp_path / "default_config.cfg"
+    filepath = tmp_path / "config.cfg"
     yield filepath
 
 
-@pytest.fixture
-def config_reader(filepath: Path) -> Generator:
-    yield ConfigReader(filepath=filepath)
-
-
-def test_copy_default_file(filepath, config_reader) -> None:
+def test_copy_default_file(filepath) -> None:
+    config_reader = ConfigReader(filepath=filepath)
     assert filepath.is_file()
 
 
-def test_file_already_existing():
-    pass
+def test_file_already_existing(filepath):
+    testfile = "tests/resources/config_dummy.cfg"
+    with open(testfile, 'r') as reader:
+        content_before = reader.readlines()
+    copyfile(testfile, filepath)
+    config_reader = ConfigReader(filepath=filepath)
+    with open(filepath, 'r') as reader:
+        content_after = reader.readlines()
+    assert content_after == content_before
 
 
-def test_read_single_value(config_reader):
+def test_read_single_value(filepath):
+    copyfile("tests/resources/config_dummy.cfg", filepath)
+    config_reader = ConfigReader(filepath=filepath)
     value = config_reader.get_config_value_single(
-        section="database",
-        option="dbms")
-    assert value == "sqlite"
+        section="section_1",
+        option="option_11")
+    assert value == "MyString"
 
 
-def test_read_list_value(config_reader):
+def test_read_list_value(filepath):
+    copyfile("tests/resources/config_dummy.cfg", filepath)
+    config_reader = ConfigReader(filepath=filepath)
     value = config_reader.get_config_value_list(
-        section="tws_connector",
-        option="non_systemic_codes")
-    assert value == ["162", "200", "354", "2104", "2106", "2107", "2158"]
+        section="section_2",
+        option="option_21")
+    assert value == ["11", "22", "Something", "33.4"]
