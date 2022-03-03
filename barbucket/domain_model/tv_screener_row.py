7b29146505ed1619cc50ca58cab8665642532766
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2022-03-03 14:24:18 +0100

    moved 'tv_screener_row' to 'domain_mod' directory

---
+++ barbucket/domain_model/tv_screener_row.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class TvScreenerRow():
+    ticker: str
+    exchange: str
+    country: str
+    market_cap: Optional[int]
+    avg_vol_30_in_curr: Optional[int]
+    employees: Optional[int]
+    profit: Optional[int]
+    revenue: Optional[int]
