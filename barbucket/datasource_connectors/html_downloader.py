31a81755d0f3c3c4ab3ffdfa8b043bd1bf91a8fc
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2022-03-11 00:45:02 +0100

    renamed html_downloader

--- barbucket/datasource_connectors/html_downloader.py
+++ barbucket/datasource_connectors/html_downloader.py
@@ -3,19 +3,19 @@ import requests
 from barbucket.domain_model.types import Api, Exchange, ApiNotationTranslator
 
 
-class ExchangeistingDownloader():
+class HtmlDownloader():
+    def __init__(self, api_notation_translator: ApiNotationTranslator) -> None:
+        self._api_notation_translator = api_notation_translator
 
-    @classmethod
-    def get_weblisting_singlepage(cls, exchange: Exchange) -> str:
-        ex = ApiNotationTranslator.get_api_notation_for_exchange(
+    def get_weblisting_singlepage(self, exchange: Exchange) -> str:
+        ex = self._api_notation_translator.get_api_notation_for_exchange(
             exchange=exchange, api=Api.IB)
         url = (f"https://www.interactivebrokers.com/en/index.php?f=567&exch="
                f"{ex}")
         return requests.get(url).text
 
-    @classmethod
-    def get_weblisting_multipage(cls, exchange: Exchange, page: int) -> str:
-        ex = ApiNotationTranslator.get_api_notation_for_exchange(
+    def get_weblisting_multipage(self, exchange: Exchange, page: int) -> str:
+        ex = self._api_notation_translator.get_api_notation_for_exchange(
             exchange=exchange, api=Api.IB)
         url = (f"https://www.interactivebrokers.com/en/index.php?f=2222"
                f"&exch={ex}&showcategories=STK&p=&cc=&limit="
