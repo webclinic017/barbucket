a38ae38ee4ce39cec5e6eb7b165c569f7e55a12a
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2022-03-11 00:46:27 +0100

    changed class methods to instance methods

--- barbucket/business_logic/contract_details_tv_processor.py
+++ barbucket/business_logic/contract_details_tv_processor.py
@@ -10,42 +10,35 @@ from barbucket.domain_model.types import ContractType
 
 
 class ContractDetailsTvProcessor():
-    _files_reader: TvFilesReader
-    _details_db_manager: ContractDetailsTvDbManager
-    _contracts_db_manager: ContractsDbManager
-    _session: Session
-
     def __init__(self,
                  files_reader: TvFilesReader,
                  details_db_manager: ContractDetailsTvDbManager,
                  contracts_db_manager: ContractsDbManager,
                  session: Session) -> None:
-        ContractDetailsTvProcessor._files_reader = files_reader
-        ContractDetailsTvProcessor._details_db_manager = details_db_manager
-        ContractDetailsTvProcessor._contracts_db_manager = contracts_db_manager
-        ContractDetailsTvProcessor._session = session
+        self._files_reader = files_reader
+        self._details_db_manager = details_db_manager
+        self._contracts_db_manager = contracts_db_manager
+        self._session = session
 
-    @classmethod
-    def update_ib_contract_details(cls) -> None:
+    def update_ib_contract_details(self) -> None:
         pb_manager = enlighten.get_manager()  # Setup progress bar
         progress_bar = pb_manager.counter(
             total=0, desc="Contracts", unit="contracts")
-        sceener_rows = cls._files_reader.get_all_rows()
+        sceener_rows = self._files_reader.get_all_rows()
         progress_bar.total = len(sceener_rows)
         for row in sceener_rows:
-            cls._handle_row(row=row)
+            self._handle_row(row=row)
             progress_bar.update(inc=1)
-        cls._session.close()
+        self._session.close()
 
-    @classmethod
-    def _handle_row(cls, row: TvScreenerRow) -> None:
+    def _handle_row(self, row: TvScreenerRow) -> None:
         # Check if matching contract exists in db
         contract_filters = (
             Contract.exchange == row.exchange,
             Contract.contract_type == ContractType.STOCK.name,
             Contract.exchange_symbol == row.ticker_symbol)
         try:
-            contract = cls._contracts_db_manager.get_one_by_filters(
+            contract = self._contracts_db_manager.get_one_by_filters(
                 filters=contract_filters)
         except NoResultFound:
             pass  # log
@@ -61,5 +54,5 @@ class ContractDetailsTvProcessor():
                 employees=row.employees,
                 profit=row.profit,
                 revenue=row.revenue)
-            cls._details_db_manager.write_to_db(details=details)
-            cls._session.commit()
+            self._details_db_manager.write_to_db(details=details)
+            self._session.commit()
