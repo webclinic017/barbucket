7b25fc9cc7891a1db13ad2b1acb1ad22024c7f3d
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2022-03-11 00:43:19 +0100

    added logging_preparator

---
+++ barbucket/main/logging_preparator.py
@@ -0,0 +1,40 @@
+import logging
+import logging.handlers
+from pathlib import Path
+
+
+class LoggingPreparator():
+    def _create_directories(self) -> None:
+        Path.mkdir(Path.home() / ".barbucket/tv_screener",
+                   parents=True, exist_ok=True)
+        Path.mkdir(Path.home() / ".barbucket/logs",
+                   parents=True, exist_ok=True)
+
+    def setup_logging(self) -> None:
+
+        def my_file_namer(filename):
+            new_name = filename.replace(".log.", "_") + ".log"
+            return new_name
+
+        root_logger = logging.getLogger()
+        root_logger.setLevel(logging.DEBUG)
+
+        # console_handler = logging.StreamHandler()
+        # console_handler.setLevel(logging.INFO)
+        # console_formatter = logging.Formatter("%(message)s")
+        # console_handler.setFormatter(console_formatter)
+        # root_logger.addHandler(console_handler)
+
+        file_handler = logging.handlers.TimedRotatingFileHandler(
+            filename=Path.home() / ".barbucket/logs/logfile.log",
+            when='midnight')
+        file_handler.namer = my_file_namer
+        file_handler.setLevel(logging.DEBUG)
+        file_formatter = logging.Formatter(
+            "%(asctime)s | %(name)s | %(levelname)s | %(message)s")
+        file_handler.setFormatter(file_formatter)
+        root_logger.addHandler(file_handler)
+
+        logger = logging.getLogger(__name__)
+        logger.debug(
+            "--------------------------------------- Application started")
