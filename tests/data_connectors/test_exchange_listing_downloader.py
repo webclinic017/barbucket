ac617dc714e20fbdb33c3fe825cb68d87f4e5492
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2022-03-11 00:39:41 +0100

    renamed_downloader

--- tests/data_connectors/test_exchange_listing_downloader.py
+++ tests/data_connectors/test_exchange_listing_downloader.py
@@ -2,8 +2,8 @@ from logging import getLogger
 
 import requests
 
-from barbucket.datasource_connectors.exchange_listing_downloader import ExchangeistingDownloader
-from barbucket.domain_model.types import Exchange
+from barbucket.datasource_connectors.html_downloader import HtmlDownloader
+from barbucket.domain_model.types import Exchange, ApiNotationTranslator
 
 _logger = getLogger(__name__)
 _logger.debug(f"--------- ---------- Testing ExchangeListingDownloader")
@@ -20,7 +20,8 @@ def test_get_weblisting_singlepage(monkeypatch) -> None:
     """ """
     _logger.debug(f"---------- Test: test_get_weblisting_singlepage")
     monkeypatch.setattr(requests, "get", mock_get)
-    downloader = ExchangeistingDownloader()
+    downloader = HtmlDownloader(
+        api_notation_translator=ApiNotationTranslator())
     expected_html = "https://www.interactivebrokers.com/en/index.php?f=567&exch=IBIS"
     actual_html = downloader.get_weblisting_singlepage(exchange=Exchange.XETRA)
     assert actual_html == expected_html
@@ -30,7 +31,8 @@ def test_get_weblisting_multipage(monkeypatch) -> None:
     """ """
     _logger.debug(f"---------- Test: test_get_weblisting_multipage")
     monkeypatch.setattr(requests, "get", mock_get)
-    downloader = ExchangeistingDownloader()
+    downloader = HtmlDownloader(
+        api_notation_translator=ApiNotationTranslator())
     expected_html = ("https://www.interactivebrokers.com/en/index.php?f=2222"
                      "&exch=IBIS&showcategories=STK&p=&cc=&limit=100&page=5")
     actual_html = downloader.get_weblisting_multipage(
