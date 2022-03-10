aff25a6791220f3fb6baddc894d559d2e31f91d9
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2022-03-11 00:46:42 +0100

    changed class methods to instance methods

--- barbucket/business_logic/contract_details_ib_processor.py
+++ barbucket/business_logic/contract_details_ib_processor.py
@@ -8,44 +8,38 @@ from barbucket.domain_model.data_classes import Contract
 
 
 class ContractDetailsIbProcessor():
-    _tws_connector: TwsConnector
-    _details_db_manager: ContractDetailsIbDbManager
-    _contracts_db_manager: ContractsDbManager
-    _session: Session
 
     def __init__(self,
                  tws_connector: TwsConnector,
                  details_db_manager: ContractDetailsIbDbManager,
                  contracts_db_manager: ContractsDbManager,
                  session: Session) -> None:
-        ContractDetailsIbProcessor._tws_connector = tws_connector
-        ContractDetailsIbProcessor._details_db_manager = details_db_manager
-        ContractDetailsIbProcessor._contracts_db_manager = contracts_db_manager
-        ContractDetailsIbProcessor._session = session
+        self._tws_connector = tws_connector
+        self._details_db_manager = details_db_manager
+        self._contracts_db_manager = contracts_db_manager
+        self._session = session
 
-    @classmethod
-    def update_ib_contract_details(cls) -> None:
+    def update_ib_contract_details(self) -> None:
         pb_manager = enlighten.get_manager()  # Setup progress bar
         progress_bar = pb_manager.counter(
             total=0, desc="Contracts", unit="contracts")
         contract_filters = (Contract.contract_details_ib == None,)
-        contracts = cls._contracts_db_manager.get_by_filters(
+        contracts = self._contracts_db_manager.get_by_filters(
             filters=contract_filters)
         progress_bar.total = len(contracts)
-        cls._tws_connector.connect()
+        self._tws_connector.connect()
         for contract in contracts:
-            cls._handle_contract(contract=contract)
+            self._handle_contract(contract=contract)
             progress_bar.update(inc=1)
-        cls._session.close()
-        cls._tws_connector.disconnect()
+        self._session.close()
+        self._tws_connector.disconnect()
 
-    @classmethod
-    def _handle_contract(cls, contract: Contract) -> None:
+    def _handle_contract(self, contract: Contract) -> None:
         try:
-            details = cls._tws_connector.download_contract_details(
+            details = self._tws_connector.download_contract_details(
                 contract=contract)
         except RequestError as e:
             pass  # log, does this handle no and multiple results?
         else:
-            cls._details_db_manager.write_to_db(details=details)
-            cls._session.commit()
+            self._details_db_manager.write_to_db(details=details)
+            self._session.commit()
