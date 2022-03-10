862113c1460845540501eedee4a758504d017be7
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2022-03-10 18:50:59 +0100

    removed builder

--- barbucket/config/config_reader_builder.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-from barbucket.config_reader import ConfigReader
-
-
-def build() -> ConfigReader:
-    filepath = Path.home() / ".barbucket/config.cfg"
-    return ConfigReader(filepath=filepath)
