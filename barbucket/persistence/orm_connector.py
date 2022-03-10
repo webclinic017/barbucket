08cf77446e0f90ed9fe25b77836bf30fcaf9af93
Martin Creutzenberg <martin.creutzenberg@gmail.com>
2022-03-11 00:42:08 +0100

    changed class methods to instance methods

--- barbucket/persistence/orm_connector.py
+++ barbucket/persistence/orm_connector.py
@@ -3,7 +3,6 @@ from logging import getLogger
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.decl_api import DeclarativeMeta
-from sqlalchemy.engine import Engine
 
 from .connectionstring_assembler import ConnectionStringAssembler
 
@@ -14,51 +13,42 @@ _logger = getLogger(__name__)
 class OrmConnector():
     """ """
 
-    _connstring_assembler: ConnectionStringAssembler
-    _engine: Engine
-    _session: Session
-    _base_class: DeclarativeMeta
-
     def __init__(
             self,
             connstring_assembler: ConnectionStringAssembler,
             base_class: DeclarativeMeta) -> None:
-        OrmConnector._connstring_assembler = connstring_assembler
-        OrmConnector._base_class = base_class
-        OrmConnector._initialize()
+        self._connstring_assembler = connstring_assembler
+        self._base_class = base_class
+        self._initialize()
 
-    @classmethod
-    def get_session(cls) -> Session:
+    def get_session(self) -> Session:
         """_summary_
 
         :return: _description_
         :rtype: Session
         """
-        return cls._session
+        return self._session
 
     # ~~~~~~~~~~~~~~~~ private methods ~~~~~~~~~~~~~~~~
 
-    @classmethod
-    def _initialize(cls) -> None:
-        conn_string = cls._connstring_assembler.get_connection_string()
-        cls._engine = create_engine(conn_string, future=True)
-        cls._add_sqlite_pragma()
-        cls._create_db_schema()
-        cls._session = Session(cls._engine)
+    def _initialize(self) -> None:
+        conn_string = self._connstring_assembler.get_connection_string()
+        self._engine = create_engine(conn_string, future=True)
+        self._add_sqlite_pragma()
+        self._create_db_schema()
+        self._session = Session(self._engine)
 
-    @classmethod
-    def _add_sqlite_pragma(cls) -> None:
+    def _add_sqlite_pragma(self) -> None:
         # Adding foreign key pragma on every connection for sqlite by event
         def _fk_pragma_on_connect(dbapi_con, con_record):
             dbapi_con.execute('PRAGMA foreign_keys = 1')
 
-        if cls._engine.url.drivername == 'sqlite':
-            event.listen(cls._engine, 'connect', _fk_pragma_on_connect)
+        if self._engine.url.drivername == 'sqlite':
+            event.listen(self._engine, 'connect', _fk_pragma_on_connect)
             _logger.debug("Added FOREIGN_KEY pragma event for sqlite within "
-                          f"engine '{cls._engine}'.")
+                          f"engine '{self._engine}'.")
 
-    @classmethod
-    def _create_db_schema(cls) -> None:
-        cls._base_class.metadata.create_all(cls._engine)
+    def _create_db_schema(self) -> None:
+        self._base_class.metadata.create_all(self._engine)
         _logger.debug(
-            f"Created database schema within engine '{cls._engine}'.")
+            f"Created database schema within engine '{self._engine}'.")
