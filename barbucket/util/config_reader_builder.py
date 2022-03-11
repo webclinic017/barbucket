f3e2697fa5b2c73715ba84e94304cd997e9f9cf6
Martin <35747507+croidzen@users.noreply.github.com>
2022-03-11 17:43:11 +0100

    removed builder

--- barbucket/util/config_reader_builder.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-from barbucket.util.config_reader import ConfigReader
-
-
-def build() -> ConfigReader:
-    filepath = Path.home() / ".barbucket/config.cfg"
-    return ConfigReader(filepath=filepath)
