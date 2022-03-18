fec63f4917ae9812079bf1d2949faabbdf33dbff
Martin <35747507+croidzen@users.noreply.github.com>
2022-03-18 23:00:28 +0100

    updated quote replacing algo

--- barbucket/persistence/data_managers.py
+++ barbucket/persistence/data_managers.py
@@ -21,7 +21,7 @@ class UniverseDbManager():
                      .where(UniverseMembership.universe == universe))
         result = self._orm_session.execute(statement).scalars()
         members = [row.contract for row in result]
-        _logger.debug(f"Read {len(result)} members for universe '{universe}' "
+        _logger.debug(f"Read {len(members)} members for universe '{universe}' "
                       f"from database with session '{self._orm_session}'.")
         return members
 
@@ -30,19 +30,20 @@ class ContractsDbManager():
     def __init__(self, orm_session: Session) -> None:
         self._orm_session = orm_session
 
-    def is_existing(self, contract: Contract) -> bool:
-        statement = (select(Contract.id).where(and_(
-            Contract.broker_symbol == contract.broker_symbol,
-            Contract.currency == contract.currency,
-            Contract.exchange == contract.exchange)))
-        count = self._orm_session.execute(statement).count()
-        _logger.debug(f"Found {count} entries for Contract '{contract}' with "
-                      f"session '{self._orm_session}'")
-        if count > 1:
-            pass  # todo: raise exception
-        return bool(count)
+    # def is_existing(self, contract: Contract) -> bool:
+    #     statement = (select(Contract.id).where(and_(
+    #         Contract.contract_type == contract.contract_type,
+    #         Contract.broker_symbol == contract.broker_symbol,
+    #         Contract.currency == contract.currency,
+    #         Contract.exchange == contract.exchange)))
+    #     count = self._orm_session.execute(statement).count()
+    #     _logger.debug(f"Found {count} entries for Contract '{contract}' with "
+    #                   f"session '{self._orm_session}'")
+    #     if count > 1:
+    #         pass  # todo: raise exception
+    #     return bool(count)
 
-    def write_to_db(self, contract: Contract) -> None:
+    def add_to_db(self, contract: Contract) -> None:
         self._orm_session.add(contract)
         _logger.debug(f"Added Contract '{contract}' to session "
                       f"'{self._orm_session}'")
@@ -64,7 +65,7 @@ class ContractsDbManager():
                       f"'{self._orm_session}'.")
         return contracts
 
-    def delete(self, contract: Contract) -> None:
+    def delete_from_db(self, contract: Contract) -> None:
         self._orm_session.delete(contract)
         _logger.debug(f"Deleted Contract '{contract}' with session "
                       f"'{self._orm_session}'")
@@ -85,7 +86,7 @@ class ContractsDbManager():
 #                       f"'{self._orm_session}'.")
 #         return status
 
-#     def write_to_db(self, status: QuotesStatus) -> None:
+#     def add_to_db(self, status: QuotesStatus) -> None:
 #         self._orm_session.add(status)
 #         _logger.debug(f"Added QuotesStatus '{status}' to session "
 #                       f"'{self._orm_session}'")
@@ -103,21 +104,18 @@ class QuotesDbManager():
     def __init__(self, orm_session: Session) -> None:
         self._orm_session = orm_session
 
-    def write_to_db(self, quotes: List[Quote]) -> None:
+    def upsert_to_db(self, quotes: List[Quote]) -> None:
         # Needs to overwrite existing quotes
         contract = quotes[0].contract
-        existing_quotes = self._orm_session.execute(
-            select(Quote).where(Quote.contract == contract)).scalars().all()
+        dates = [quote.date for quote in quotes]
+        conflicted_quotes = self._orm_session.execute(
+            select(Quote).where(
+                and_(Quote.contract == contract, Quote.date.in_(dates)))
+        ).scalars().all()
+        self._orm_session.add(conflicted_quotes)
         for quote in quotes:
-            if quote in existing_quotes:
-                quote_to_update = self._orm_session.execute(select(Quote).
-                                                            where(and_(Quote.contract == contract,
-                                                                       Quote.date == quote.date)))
-                quote_to_update.open = quote.open
-                quote_to_update.high = quote.high
-                quote_to_update.low = quote.low
-                quote_to_update.close = quote.close
-                quote_to_update.volume = quote.volume
+            if quote in conflicted_quotes:
+                self._orm_session.merge(quote)
             else:
                 self._orm_session.add(quote)
         _logger.debug(f"Added {len(quotes)} quotes to session "
@@ -143,7 +141,7 @@ class ContractDetailsIbDbManager():
     def __init__(self, orm_session: Session) -> None:
         self._orm_session = orm_session
 
-    def write_to_db(self, details: ContractDetailsIb) -> None:
+    def add_to_db(self, details: ContractDetailsIb) -> None:
         self._orm_session.add(details)
         _logger.debug(f"Added ContractDetailsIb '{details}' to session "
                       f"'{self._orm_session}'")
@@ -153,7 +151,7 @@ class ContractDetailsTvDbManager():
     def __init__(self, orm_session: Session) -> None:
         self._orm_session = orm_session
 
-    def write_to_db(self, details: ContractDetailsTv) -> None:
+    def add_to_db(self, details: ContractDetailsTv) -> None:
         self._orm_session.add(details)
         _logger.debug(f"Added ContractDetailsTv '{details}' to session "
                       f"'{self._orm_session}'")
