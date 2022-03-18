bf9d6b8c48172ab25590a4c2dec66f2077b783f7
Martin <35747507+croidzen@users.noreply.github.com>
2022-03-18 22:58:19 +0100

    updated contracts syncing algo

--- barbucket/business_logic/contracts_sync_processor.py
+++ barbucket/business_logic/contracts_sync_processor.py
@@ -1,4 +1,5 @@
 import logging
+import datetime
 
 from sqlalchemy.orm import Session
 
@@ -20,6 +21,7 @@ class ContractSyncProcessor():
         self._listing_reader = listing_reader
         self._contracts_db_manager = contracts_db_manager
         self._orm_session = orm_session
+        self.override_user_acknowledge = False
 
     def sync_contracts_to_listing(self, exchange: Exchange) -> None:
 
@@ -35,24 +37,31 @@ class ContractSyncProcessor():
             Contract.exchange == exchange.name)
         db_contracts = self._contracts_db_manager.get_by_filters(
             filters=contract_filters)
-
-        # Find removed contracts
-        removed_contracts = []
-        for contract in db_contracts:
-            if contract not in web_contracts:
-                self._orm_session.delete(contract)
-                removed_contracts.append(contract)
-
-        # Find addded contracts
-        added_contracts = []
-        for contract in web_contracts:
-            if contract not in db_contracts:
-                self._orm_session.add(contract)
-                added_contracts.append(contract)
+        wc = set(web_contracts)
+        dc = set(db_contracts)
+        removed_contracts = dc.difference(wc)
+        added_contracts = wc.difference(dc)
 
         # Execute
-        if True:  # User acknowledge
+        if not self.override_user_acknowledge:
+            ua = self._user_acknowledge(n_remove=len(removed_contracts),
+                                        n_add=len(added_contracts))
+        if self.override_user_acknowledge or ua:
+            for contract in removed_contracts:
+                self._contracts_db_manager.delete_from_db(contract=contract)
+            for contract in added_contracts:
+                self._contracts_db_manager.add_to_db(contract=contract)
             self._orm_session.commit()
         else:
-            self._orm_session.rollback()
+            _logger.info("Aborted.")
         self._orm_session.close()
+
+    def _user_acknowledge(self, n_remove: int, n_add: int) -> bool:
+        print(f"{n_add} new contracts will be added and {n_remove} deprecated "
+              f"contracts will be removed. Do you want to continue? (Y/N): ",
+              end="")
+        message = input()
+        if message.upper() == "Y":
+            return True
+        else:
+            return False
