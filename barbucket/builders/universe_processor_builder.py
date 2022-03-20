a5d785a7009fbcffff1a5054251248dec011bd7b
Martin <35747507+croidzen@users.noreply.github.com>
2022-03-20 13:08:43 +0100

    changed builder to universe_processor

--- barbucket/builders/universe_processor_builder.py
+++ barbucket/builders/universe_processor_builder.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 
+from barbucket.business_logic.universe_processor import UniverseProcessor
 from barbucket.domain_model.data_classes import Base
 from barbucket.persistence.connectionstring_assembler import ConnectionStringAssembler
-from barbucket.persistence.data_managers import UniverseDbManager
+from barbucket.persistence.data_managers import ContractsDbManager, UniverseDbManager
 from barbucket.persistence.orm_connector import OrmConnector
 from barbucket.util.config_reader import ConfigReader
 
 
-def build_universe_db_manager():
+def build_universe_processor():
     config_reader = ConfigReader(
         filepath=Path.home() / ".barbucket/config.cfg")
 
@@ -20,4 +21,11 @@ def build_universe_db_manager():
 
     universe_db_manager = UniverseDbManager(orm_session=orm_session)
 
-    return universe_db_manager
+    contracts_db_manager = ContractsDbManager(orm_session=orm_session)
+
+    universe_processor = UniverseProcessor(
+        universe_db_manager=universe_db_manager,
+        contracts_db_manager=contracts_db_manager,
+        orm_session=orm_session)
+
+    return universe_processor
