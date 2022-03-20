528da349c21f5096b5843f9761207e0aa2c1d908
Martin <35747507+croidzen@users.noreply.github.com>
2022-03-20 13:10:34 +0100

    updated logging

--- barbucket/datasource_connectors/html_corrector.py
+++ barbucket/datasource_connectors/html_corrector.py
@@ -18,13 +18,11 @@ class HtmlCorrector():
                 corrections += 1
             new_lines.append(line)
         html = "\n".join(new_lines)
-        if corrections == 0:
-            _logger.debug(
-                f"IB error for singlepage listings no longer present. Checked "
-                f"{len(old_lines)} lines.")
+        if corrections > 0:
+            _logger.debug(f"Corrected IB HTML error in {corrections} lines.")
         else:
-            # log
-            pass
+            _logger.debug(f"IB error for singlepage listings no longer "
+                          f"present. Checked {len(old_lines)} lines.")
         return html
 
     def correct_ib_error_multipage(self, html: str) -> str:
@@ -36,7 +34,6 @@ class HtmlCorrector():
                 "(click link for more details)</span></th>\n                       </th>\n",
                 "(click link for more details)</span></th>\n")
         else:
-            _logger.debug(
-                f"IB error for paginated listings no longer present. Checked "
-                f"{len(html.splitlines())} lines.")
+            _logger.debug(f"IB error for paginated listings no longer "
+                          f"present. Checked {len(html.splitlines())} lines.")
         return html
